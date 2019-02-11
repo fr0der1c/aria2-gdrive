4a1f1bbbe23bf41ed89b40013503e625b0976944
Frederic <frederic.t.chan@gmail.com>
2019-02-11 16:41:33 +0800

    try daemon mode

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rclone mount my-drive:/ /data/GoogleDrive --allow-other --allow-non-empty --vfs-cache-mode writes &
+rclone mount my-drive:/ /data/GoogleDrive --allow-other --allow-non-empty --vfs-cache-mode writes --daemon
 nginx
 aria2c
