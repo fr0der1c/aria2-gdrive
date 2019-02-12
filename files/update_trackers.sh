2de8de504ed80b903464310c15bd41ec9d3ef643
Frederic Chan <frederic.t.chan@gmail.com>
2019-02-12 10:49:43 +0800

    fix sh

--- files/update_trackers.sh
+++ files/update_trackers.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/usr/sbin/service aria2 stop
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [[ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
