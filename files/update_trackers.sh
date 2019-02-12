322effba52ac750ecfd427049f24cd0a33611956
Frederic Chan <frederic.t.chan@gmail.com>
2019-02-12 10:33:31 +0800

    switch tracker list to best

--- files/update_trackers.sh
+++ files/update_trackers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/sbin/service aria2 stop
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [[ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
     echo "Add bt-trackers to aria2.conf"
